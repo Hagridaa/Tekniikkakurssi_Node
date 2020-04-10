08e23ef2a9c23c90f02ca1107490763f1fb08be1
hagridaa <annekristiinarasanen@gmail.com>
2020-04-10 17:36:23 +0300

    added excercise3 post ja html response

--- viikko2/app.js
+++ viikko2/app.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+app.use(express.json())
+
+app.set('view engine', 'pug')
+app.set('views', 'public')
+app.use(express.urlencoded({extended: true}))
 
 
 // app.get('/', (req, res) =>
 //     res.send('Hello World!'))
-
 app.use(express.static('public'))
 
 app.get('/api/exercise', (req, res) => {
-
     console.log(req.query)
     res.send(req.query)
-    // for (const key in req.query) {
-    //     console.log(key, req.query[key])
-    //
-    // }
-    // res.send([key,req.query[key]])
     res.status(200);
 })
 
+app.post('/api/exercise', (req, res) => res.render('list', {items: req.body}).status(200))
+
 app.listen(port, () =>
-    console.log(`Example app listening on
-port ${port}!`))
+    console.log(`Example app listening on port ${port}!`))
