92986197be4dc165b3c9c9815330e796e3764a5d
hagridaa <annekristiinarasanen@gmail.com>
2020-04-12 16:34:22 +0300

    Added http-status and cors

--- viikko2/app.js
+++ viikko2/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path')
 app.use(express.json())
+const cors = require('cors')
 
 app.set('view engine', 'pug')
 app.set('views', 'public')
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 
 // app.get('/', (req, res) =>
@@ -19,7 +20,34 @@ app.get('/api/exercise', (req, res) => {
     res.status(200);
 })
 
-app.post('/api/exercise', (req, res) => res.render('list', {items: req.body}).status(200))
+app.post('/api/exercise', (req, res) => {
+    res.render('list', {items: req.body})
+    res.status(200);
+})
+
+app.post('/api/login', (req, res) => {
+    const username = req.body.user
+    const password = req.body.pwd
+
+    if(username === '' || password === '') {
+        res.sendStatus(400);
+    }
+    else if (username === 'mark' && password === 'giraffe') {
+        res.json({user: username}).status(200)
+    }
+    else {
+        res.sendStatus(403)
+    }
+})
+
+app.get('/api', (req, res) => {
+    res.json({msg: "Hello, world!"}).status(200)
+})
+
+app.get('/hello', (req, res) => {
+    res.render('hello')
+    res.status(200);
+})
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`))
